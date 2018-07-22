684515cc64da7ebf2c7d8c2430ed1432f6341d7c
Samuel <samramox@gmail.com>
2018-07-21 23:52:58 -0300

    Feat: added style

--- components/AddDeck/index.js
+++ components/AddDeck/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const AddDeck = () => (
   <View style={styles.container}>
     <Text>Add Deck</Text>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 export default AddDeck;
--- components/Decks/index.js
+++ components/Decks/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const Decks = () => (
   <View style={styles.container}>
     <Text>Decks</Text>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 export default Decks;
